9111f753019f31078e4a787a8b3e14f898abff49
Aiert Basante <otxoboy64@gmail.com>
2024-10-30 17:28:17 +0100

    no tengo muy claro como seguir

--- backend/live_chat/consumers.py
+++ backend/live_chat/consumers.py
@@ -3,6 +3,7 @@ import uuid
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+from .models import Room, Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -14,21 +15,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
     
     async def disconnect(self , close_code):
-        # leave room
+        # Leave room
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
-
+    
+    # Receive message from WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        username = text_data_json["username"]
-        await self.channel_layer.group_send(
-            self.roomGroupName,{
-                "type" : "sendMessage" ,
-                "message" : message , 
-                "username" : username ,
-            })
+        data = json.loads(text_data)
+        print(data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
 
-    async def sendMessage(self , event) : 
-        message = event["message"]
-        username = event["username"]
-        await self.send(text_data = json.dumps({"message":message ,"username":username}))
+        await self.save_message(username, room, message)
+
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username
+            }
+        )
+
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username
+        }))
+
+    @sync_to_async
+    def save_message(self, username, room, message):
+        user = User.objects.get(username=username)
+        room = Room.objects.get(slug=room)
+
+        Message.objects.create(user=user, room=room, content=message)
