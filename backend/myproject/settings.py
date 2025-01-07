8b9d5a377a0bda321f63538172fa558c1b9a3fac
otxobot <otxoboy64@gmail.com>
2025-01-07 17:12:14 +0100

    añadi la secret key en setting.py

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -14,6 +14,8 @@ import os
 from pathlib import Path
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
+
 
 load_dotenv()
 
@@ -26,10 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-
+SECRET_KEY = get_random_secret_key()
 #SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = bool(os.environ.get("DEBUG", default=0))
 DEBUG = True
