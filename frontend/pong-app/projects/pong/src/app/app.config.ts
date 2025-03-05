e82c3efca1676bb6cdad48fa915385f0f670cc35
lagonzal <larraingonzalez@gmail.com>
2025-03-05 12:25:20 +0100

    Translations now working with nginx

--- frontend/pong-app/projects/pong/src/app/app.config.ts
+++ frontend/pong-app/projects/pong/src/app/app.config.ts
@@ -1,6 +1,5 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -8,27 +7,27 @@ import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-
-const HttpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
-  new TranslateHttpLoader(http, './i18n/', '.json');
-
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-
-    provideZoneChangeDetection({ eventCoalescing: true}),
     provideHttpClient(withFetch(), withInterceptors([AuthInterceptor])),
-    importProvidersFrom([TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory, 
-        deps: [HttpClient],
-      },
-    })])
-  ],
-}
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
+  ]
+};
 
 // export const appConfig: ApplicationConfig = {
 //   providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch(), withInterceptors([AuthInterceptor]))]
