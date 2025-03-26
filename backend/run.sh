4cea55829ccc706728f7d26457a886004dad7c73
lagonzal <larraingonzalez@gmail.com>
2025-03-26 22:03:53 +0100

    Changes to the backend entrypoint to make migrations for all the apps

--- backend/run.sh
+++ backend/run.sh
@@ -6,11 +6,20 @@ pip install --upgrade pip
 
 cd /code/backend/
 
-python manage.py makemigrations
+ls
+
+# First create migrations for specific apps if they don't exist
+for app in accounts live_chat two_factor_auth; do
+  if [ ! -f "${app}/migrations/0001_initial.py" ]; then
+    echo "Creating initial migrations for ${app}"
+    python manage.py makemigrations ${app}
+  fi
+done
+
 python manage.py migrate
 
 # Create superuser
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', 'admin') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell
+# echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@gmail.com', 'admin') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell
 
 # run server
 python manage.py runserver 0.0.0.0:8000
