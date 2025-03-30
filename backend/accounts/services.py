f9e4216541f19335a8c7f8a71f27cde642228495
inozo22 <119419194+inozo22@users.noreply.github.com>
2025-03-30 15:42:19 +0200

    changed domain in send_activation_email

--- backend/accounts/services.py
+++ backend/accounts/services.py
@@ -1,27 +1,35 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import AccountActivateToken
 
-def send_activation_email(user, from_email):
+def send_activation_email(user, from_email, request=None):
     """Business logic to send an activation email."""
     email = user.email
-    
+
     account_activate_token = AccountActivateToken.objects.create_token(user=user)
     activate_token = account_activate_token.activate_token
-    
-    # Create the activation URL
-    #url = f'{settings.ACTIVATE_URL}/activation/?token={activate_token}'
-    url = f'https://localhost:8089/verify?token={activate_token}'
 
-    subject = 'Pong game : Account Activation'
-    message = f'''
+    if request:
+        domain = get_current_site(request).domain
+    else:
+        domain = "localhost:8089"  # Fallback por si `request` no está disponible
+
+    activation_url = f"https://{domain}/verify?token={activate_token}"
+
+    subject = "Pong Game: Account Activation"
+    message = f"""
     Hi {user.username},
-    This email is sent from Pong game.
-    Please click the link below to activate your account.
-    {url}
+
+    This email is sent from Pong Game.
+    Please click the link below to activate your account:
+    
+    {activation_url}
 
     If you did not request this email, please ignore it.
-
-    '''
+    """
 
     send_mail(subject, message, from_email, [email])
